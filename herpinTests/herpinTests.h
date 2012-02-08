1ee3f652e3d1e5ec4d3146d7767111005be14a79
PILSner <team6@mary008-0103-dhcp251.bu.edu>
2012-02-08 17:56:16 -0500

    Initial Commit

---
+++ herpinTests/herpinTests.h
@@ -0,0 +1,13 @@
+//
+//  herpinTests.h
+//  herpinTests
+//
+//  Created by team6 on 2/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface herpinTests : SenTestCase
+
+@end
---
+++ herpinTests/herpinTests.m
@@ -0,0 +1,32 @@
+//
+//  herpinTests.m
+//  herpinTests
+//
+//  Created by team6 on 2/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "herpinTests.h"
+
+@implementation herpinTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in herpinTests");
+}
+
+@end
